0863488957e6195b5fff44a984d9b5c6fc55aab3
anna_topping <itsannatopping@gmail.com>
2026-04-14 11:23:39 -0400

    Initial Commit

---
+++ ioscamera_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ioscamera_project
+//
+//  Created by Anna Topping on 4/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ioscamera_project/ioscamera_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ioscamera_projectApp.swift
+//  ioscamera_project
+//
+//  Created by Anna Topping on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ioscamera_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
